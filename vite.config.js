bb3bcd1a3ac88218bb86dd4a33eb4f8e6404aca6
PhantasyXing <barcelonawind@qq.com>
2025-12-24 22:02:48 +0800

    element主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,7 +16,10 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1.配置eLemertPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" })
+      ],
     }),
   ],
   resolve: {
@@ -25,4 +27,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
