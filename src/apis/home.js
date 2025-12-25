b8635fdb9af2038c09ccffbee97022ff34b1d3f9
PhantasyXing <barcelonawind@qq.com>
2025-12-25 22:13:21 +0800

    home-新鲜好物和人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,21 @@ export function getBannerAPI() {
     url: 'home/banner'
   })
 }
+
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url: '/home/new'
+  })
+}
+
+export const getHotAPI = () => {
+  return httpInstance({
+    url: '/home/hot'
+  })
+}
