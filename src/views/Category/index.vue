d43a39add767aacd85e3e0c93dc3901f52fa798e
PhantasyXing <barcelonawind@qq.com>
2025-12-27 12:06:32 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,34 +1,10 @@
 <script setup>
-import {getTopCategoryAPI} from '@/apis/category'
-import { getBannerAPI } from '@/apis/home'
-import { onMounted, ref } from 'vue';
-import {onBeforeRouteUpdate, useRoute} from 'vue-router'
 import GoodsItem from '../Home/components/GoodsItem.vue';
+import { useBanner } from './composables/useBanner'
+import { useCategory } from './composables/useCategory'
+const {bannerList} = useBanner()
+const {categoryData} = useCategory()
 
-// 获取数据
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async (id = route.params.id)=>{
-  const res = await getTopCategoryAPI(id)
-  categoryData.value = res.result
-}
-onMounted( () => getCategory())
-
-// 目标：路由参数变化的时候 可以把分类数据接口重新发送
-onBeforeRouteUpdate((to)=>{
-    getCategory(to.params.id)
-})
-
-// 获取轮播图
-const bannerList = ref([])
-const getBanner = async ()=>{
-  const res = await getBannerAPI({
-    distributionSite:'2'
-  })
-  // console.log(res)
-  bannerList.value = res.result
-}
-onMounted( () => getBanner())
 
 </script>
 
