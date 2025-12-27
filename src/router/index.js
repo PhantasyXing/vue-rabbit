8c809bdf7ef44f58236ad03b035a79fe204a2440
PhantasyXing <barcelonawind@qq.com>
2025-12-27 22:51:19 +0800

    详情页-整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Login from '@/views/Login/index.vue'
 import Category from '@/views/Category/index.vue'
 import Home from '@/views/Home/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,12 +21,16 @@ const router = createRouter({
           component: Home
         },
         {
-          path: 'Category/:id',
+          path: 'category/:id',
           component: Category
         },
         {
-          path: 'Category/sub/:id',
+          path: 'category/sub/:id',
           component: SubCategory
+        },
+        {
+          path: 'detail/:id',
+          component: Detail
         },
       ]
     },
@@ -34,6 +39,13 @@ const router = createRouter({
       component: Login
     },
   ],
+  // 路由滚动行为定制
+  scrollBehavior() {
+    return {
+      top: 0
+    }
+  }
 })
+
 
 export default router
