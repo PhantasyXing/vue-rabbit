bbdffb467c8f4b6b9cf2e9710bdfcb884c92a9d1
PhantasyXing <barcelonawind@qq.com>
2025-12-28 21:36:57 +0800

    详情热榜-title和列表内容的适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,14 +1,27 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { getHotGoodsAPI } from '@/apis/detail'
 import { useRoute} from 'vue-router'
+
+// 适配 title , 1 - 24小时热榜 2 - 周热榜
+const props = defineProps({
+  hotType:{
+    type : Number
+  }
+})
+const TYPEMAP = {
+  1:'24小时热榜',
+  2:'周热榜'
+}
+const title = computed(()=> TYPEMAP[props.hotType])
+
 
 const route = useRoute()
 const hotList = ref([])
 const getHotList = async ()=>{
   const res = await getHotGoodsAPI({
     id: route.params.id,
-    type: 1
+    type: props.hotType
   })
   hotList.value = res.result
 }
@@ -19,7 +32,7 @@ onMounted(() => getHotList())
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{title}}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
