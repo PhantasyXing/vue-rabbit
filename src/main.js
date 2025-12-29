2987e7347cd1f4dff02abbe3e12482be33aaf39d
PhantasyXing <barcelonawind@qq.com>
2025-12-29 16:11:13 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { lazyPlugin } from '@/directives'
-
 import App from './App.vue'
 import router from './router'
+
+// 引入懒加载指令插件并注册
+import { lazyPlugin } from '@/directives'
+// 引入全局插件
+import { componentPlugin } from '@/components/index'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 
 app.mount('#app')
 
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,9 +3,6 @@ import { getDetailAPI } from '@/apis/detail'
 import { onMounted, ref } from 'vue';
 import {useRoute} from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
-import ImageView from '@/components/ImageView/index.vue'
-import XtxSku from '@/components/XtxSku/index.vue'
-
 
 const route = useRoute()
 const goods = ref({})
@@ -46,7 +43,7 @@ const skuChang = (sku)=>{
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures"></ImageView>
+              <XtxImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
