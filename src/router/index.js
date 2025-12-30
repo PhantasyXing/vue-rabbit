c6073bc1b8073fb48d9d367b417c3c622b29b2cf
PhantasyXing <barcelonawind@qq.com>
2025-12-30 22:29:47 +0800

    结算页-路由配置以及基础数据的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Home from '@/views/Home/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,8 +35,12 @@ const router = createRouter({
           component: Detail
         },
         {
-          path: 'cartList',
+          path: 'cartlist',
           component: CartList
+        },
+        {
+          path: 'checkout',
+          component: Checkout
         },
       ]
     },
