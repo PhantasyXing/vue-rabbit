15196979c60bfb3ab7b220b51ed56c6ffb29f4f8
PhantasyXing <barcelonawind@qq.com>
2025-12-31 16:51:23 +0800

    会员中心-功能理解以及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import payBack from '@/views/Pay/PayBack.vue'
+import Menber from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +54,20 @@ const router = createRouter({
         {
           path: 'paycallback',
           component: payBack
+        },
+        {
+          path: 'member',
+          component: Menber,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            },
+          ]
         }
       ]
     },
